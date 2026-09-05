861f23de713b8b4c0efebb32d5a847e29fa30fd2
csigezoltan <274210146+csigezoli@users.noreply.github.com>
2026-09-05 14:08:56 +0200

    Adding swagger to dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -22,11 +22,13 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-h2console")
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-security-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.1")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("tools.jackson.module:jackson-module-kotlin")
     runtimeOnly("com.h2database:h2")
